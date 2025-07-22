97ce0b53446c0540f6245fd508497d5e68cdb835
김선화 <ssun134568@gmail.com>
2025-07-23 08:58:11 +0900

    이모지 - 에셋 파일 정리 / 컴포넌트 정리

---
+++ src/components/selectBox.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div>select box</div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import resultBox from '../components/resultBox.vue'
+import selectBox from '../components/selectBox.vue'
+</script>
+
+<template>
+  <main>
+    <resultBox />
+    <selectBox />
+  </main>
+</template>
